8e861f8addc57c34c76262cce6a949fb8f1cf146
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-08 10:23:48 +0300

    install tailwind css

--- src/app/head.js
+++ src/app/head.js
@@ -3,7 +3,7 @@ export default function head() {
     <>
       <title>Google Clone</title>
       <meta name='description' content='Google clone using next js 13' />
-      <link rel='icon' href='/favicon.ico' />
+      <link rel='icon' href='favicon.svg' />
     </>
   )
 }
