d54d240e3ca40028e0134d37beaa01614be4eb31
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-08 10:57:03 +0300

    add home header

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
+import HomeHeader from '@/components/HomeHeader'
+
 const Home = () => {
   return (
     <>
-      <div>
-        <h1 className='text-red-500'>Home</h1>
-      </div>
+      <>
+        <HomeHeader />
+      </>
     </>
   )
 }
---
+++ src/components/HomeHeader.js
@@ -0,0 +1,23 @@
+import Link from 'next/link'
+import { TbGridDots } from 'react-icons/tb'
+
+const HomeHeader = () => {
+  return (
+    <>
+      <header className='p-4'>
+        <div className=' flex justify-end text-sm space-x-4 items-center'>
+          <Link href='https://mail.google.com'>Gmail</Link>
+          <Link href='https://images.google.com'>Image</Link>
+          <TbGridDots className='bg-transparent hover:bg-gray-100 rounded-full text-4xl p-2' />
+          <Link href='https://accounts.google.com/ServiceLogin/signinchooser?elo=1&flowName=GlifWebSignIn&flowEntry=ServiceLogin'>
+            <button className='bg-blue-500 hover:bg-blue-700 hover:shadow-md transition-shadow text-white  py-2 px-5 rounded-md font-medium '>
+              Sign in
+            </button>
+          </Link>
+        </div>
+      </header>
+    </>
+  )
+}
+
+export default HomeHeader
