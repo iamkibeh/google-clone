30048aac891283a47749e31dc6409909e5e848df
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-08 09:20:03 +0300

    Create next js app and cleanup

---
+++ src/app/head.js
@@ -0,0 +1,9 @@
+export default function head() {
+  return (
+    <>
+      <title>Google Clone</title>
+      <meta name='description' content='Google clone using next js 13' />
+      <link rel='icon' href='/favicon.ico' />
+    </>
+  )
+}
