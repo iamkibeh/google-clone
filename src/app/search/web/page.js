0654e5d539a8e50b0e8fa287d69ccd9f37d583c2
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-10 17:15:53 +0300

    create a custom error page

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,15 +1,38 @@
+import Link from 'next/link'
+
 const WebSearchPage = async ({ searchParams }) => {
   const searchTerm = searchParams['search-term']
 
-  console.log(searchTerm)
+  // console.log(searchTerm)
   const response = await fetch(
     `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}&q=${searchTerm}`
   )
+
+  if (!response.ok) {
+    throw new Error('Something went wrong!')
+  }
   const data = await response.json()
 
   // console.log(data)
 
   const results = data.items
+
+  if (!results) {
+    return (
+      <div className='flex flex-col justify-center items-center p-4'>
+        <h1 className='mb-5 text-2xl '>Ooops! No results found.</h1>
+        <p className='text-sm'>
+          Try searching something else or go back home.{' '}
+          <Link
+            href='/'
+            className='text-blue-500 hover:underline cursor-pointer'
+          >
+            Home
+          </Link>
+        </p>
+      </div>
+    )
+  }
   return (
     <>
       <div className=''>
