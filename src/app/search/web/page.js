692a2adaeeb35ff0cd0ed985203be208b00c7bf3
Kibet Immanuel Ng'eno <kibetimmanuel0@gmail.com>
2023-03-10 16:36:37 +0300

    fetch data from google search api and fetch data

--- src/app/search/web/page.js
+++ src/app/search/web/page.js
@@ -1,7 +1,25 @@
-const WebSearchPage = () => {
+const WebSearchPage = async ({ searchParams }) => {
+  const searchTerm = searchParams['search-term']
+
+  console.log(searchTerm)
+  const response = await fetch(
+    `https://www.googleapis.com/customsearch/v1?key=${process.env.GOOGLE_API_KEY}&cx=${process.env.GOOGLE_SEARCH_ENGINE_ID}&q=${searchTerm}`
+  )
+  const data = await response.json()
+
+  // console.log(data)
+
+  const results = data.items
   return (
     <>
-      <div className=''>WebSearchPage</div>
+      <div className=''>
+        {results &&
+          results.map((result) => (
+            <div key={result.link} className='max-w-xl mb-8'>
+              {result.title}
+            </div>
+          ))}
+      </div>
     </>
   )
 }
